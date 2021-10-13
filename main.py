1bd7ff0540677da98de415e07701c11528313d5b
baracdavid <david_barucu10@yahoo.com>
2021-10-13 13:34:13 +0300

    Am adugat problema 17

--- main.py
+++ main.py
@@ -64,6 +64,32 @@ def test_get_longest_div_k():
     assert get_longest_div_k([2,3,4,5,1,3,12,2,4,6,8],2)==[12,2,4,6,8]
     assert get_longest_div_k([11,22,33,99],11) == [11,22,33,99]
     assert get_longest_div_k([],2)==None
+def get_media_unei_liste(lista):
+    s=0
+    for element in lista :
+        s+=element
+    return s/len(lista)
+def get_longest_average_below(lista, average):
+    list_secvente = []
+    for start in range(0, len(lista)):
+        for end in range(start + 1, len(lista) + 1):
+            for element in lista[start:end]:
+                if get_media_unei_liste(lista[start:end])< average:
+                    list_secvente.append(lista[start:end])
+    if len(list_secvente) > 0 :
+        max_sec=[]
+        for secvente in list_secvente :
+            if len(secvente) > len(max_sec) :
+                max_sec=secvente
+        return max_sec
+    else:
+        return None
+def test_get_longest_average_below():
+    assert get_longest_average_below([1,2,3],2.5)==[1,2,3]
+    assert get_longest_average_below([10, 20 ,30, 40 ,213 ,321, 2], 30) == [10, 20, 30, 40]
+    assert get_longest_average_below([1,2,3],1)== None
+    assert get_longest_average_below([1,2,3],-10)== None
+    assert get_longest_average_below([],10)== None
 def citire_lista ():
     result_list = []
     string_list = input("Introduceti elementele listei cu spatiu intre ele :")
@@ -82,6 +108,7 @@ def main():
             1.Citire lista
             2.Cea mai lunga secventa divizibila cu k
             3.Cea mai lunga secventa care este progresie aritmetica
+            4.Cea mai lunga secventa a carei medie este mai mica decat un numar citit
             X.Inchide program
             """
         )
@@ -95,11 +122,18 @@ def main():
         elif optiune == '3':
             print("Cea mai lunga secventa care este progresie aritetica este:")
             print(get_longest_arithmetic_progression(lista))
+        elif optiune =='4' :
+            nr_citit=float(input("Dati un numar:"))
+            print("Cea mai lunga secventa a carei medie este mai mica decat un numar citit este: ")
+            print (get_longest_average_below(lista,nr_citit))
         elif optiune == 'x' :
             sfarsit = True
         else:
             print("Optiune invalida")
     test_get_longest_div_k()
     test_get_longest_arithmetic_progression()
+    test_get_longest_average_below()
+    print(get_media_unei_liste(lista))
 if __name__ == '__main__':
     main()
+    get_media_unei_liste([10,20,30])
